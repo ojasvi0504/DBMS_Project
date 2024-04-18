103492478c89b892562f73f747d23f1d047912b1
Ojasvi Doye <123376306+ojasvidoye@users.noreply.github.com>
2024-04-19 01:26:13 +0530

    Initital Commit

--- frontend/src/components/Biography.jsx
+++ frontend/src/components/Biography.jsx
@@ -11,26 +11,11 @@ const Biography = ({imageUrl}) => {
           <p>Biography</p>
           <h3>Who We Are</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            blanditiis sequi aperiam. Debitis fugiat harum ex maxime illo
-            consequatur mollitia voluptatem omnis nihil nesciunt beatae esse
-            ipsam, sapiente totam aspernatur porro ducimus aperiam nisi. Ex
-            magnam voluptatum consectetur reprehenderit fugiat recusandae aut
-            similique illum natus velit, praesentium nostrum nesciunt. Deleniti,
-            nesciunt laboriosam totam iusto!
+            We're Nisha Shetty and Ojasvi Doye, students passionate about technology and healthcare. Together, we've developed this Hospital Management System as part of our project. With our combined skills in database management, software development, and user experience design, we aim to provide hospitals with a reliable, efficient, and user-friendly solution.
           </p>
-          <p>We are all in 2024!</p>
-          <p>We are working on a MERN STACK PROJECT.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            assumenda exercitationem accusamus sit repellendus quo optio dolorum
-            corporis corrupti. Quas similique vel minima veniam tenetur
-            obcaecati atque magni suscipit laboriosam! Veniam vitae minus nihil
-            cupiditate natus provident. Ex illum quasi pariatur odit nisi
-            voluptas illo qui ipsum mollitia. Libero, assumenda?
-          </p>
-          <p>Lorem ipsum dolor sit amet!</p>
-          <p>Coding is fun!</p>
+          <br />
+          <h3>Nisha Shetty: 22108A0027</h3>
+          <h3>Ojasvi Doye: 22108B0070</h3>
         </div>
       </div>
     </>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -72,11 +72,11 @@ const Footer = () => {
             </div>
             <div>
               <MdEmail />
-              <span>zeelab@gmail.com</span>
+              <span>abc@gmail.com</span>
             </div>
             <div>
               <FaLocationArrow />
-              <span>Karachi, Pakistan</span>
+              <span>Mumbai, India</span>
             </div>
           </div>
         </div>
